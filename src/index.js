31e1b2288da3ff6e5254ebd450b3ff3fe253bbde
lucas vinicius <lucasviniciusaalves@gmail.com>
2021-11-30 11:19:05 -0300

    add: checksCreateTodosUserAvailability

--- src/index.js
+++ src/index.js
@@ -24,7 +24,11 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if ((!user.pro && user.todos.length < 10) || (user.pro)) {
+    return next()
+  }
 }
 
 function checksTodoExists(request, response, next) {
